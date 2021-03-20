a1717540b0415c12998defd8b1b4b7a64fe43d6a
Alejandro Cabrejo <alecapo@MacBook-Pro-de-Alejandro.local>
2021-03-19 21:28:26 -0500

    ya casi

--- 02-async/index.js
+++ 02-async/index.js
@@ -1,39 +1,52 @@
-class Operation {
-    constructor (){
-        console.log('desde el constructor...')
-    }
-    operacion(numero1, numero2, oper){
-        
-        const promesa = new Promise((resolve,reject)=>{
-            const N1 = numero1;
-            const N2 = numero2;
-    
-            if(oper === 'suma'){
-                console.log(N1+N2)
-            }else if(oper === 'resta'){
-                console.log(N1-N2)
-            } 
-        })
-        .then(console.log('ok'))
-        .catch(err=>console.log(err))
-    }
-    operaciones(numero1,numero2,oper){
-        this.operacion(numero1,numero2,oper)
-    }
+console.clear()
+
+let suma = require ('./suma.js');
+let resta = require ('./resta.js');
+
+
+
+function operacion(numero1,numero2,operation){	
+	
+	switch(operation){		
+		 
+		case 'suma': 
+		    let sum = new suma.Suma(numero1,numero2);
+			let resSuma = sum.resultado()
+			
+			return new Promise((resolve,reject)=>{
+				if(resSuma){
+					setTimeout(()=>{
+						resolve(resSuma)
+					},0)
+				}else{
+					reject("Error en la suma")
+				}	
+				console.log('suma')
+			})
+		
+		case 'resta':
+			console.log('resta')
+			let res = new resta.Resta(numero1,numero2);
+			let resResta = res.resultado()
+
+			// resResta = null 
+
+			return new Promise((resolve,reject)=>{
+				if(resResta){
+					setTimeout(()=>{
+						resolve(resResta)
+					},0)
+				}else{
+					reject("Error en la resta")
+				}			
+			})
+
+	}	
+}
+function operaciones(){
+	operacion(3, 2, 'resta')
+	    .then((resultado)=>console.log(resultado))
+		.catch((err)=>console.log(err))
 }
 
-const resultado = new Operation()
-
-console.log(resultado.operaciones(3,2,'suma'))
-
-// Crea 2 funciones operacion y operaciones
-
-// Operacion recibirá dos números y un string con el nombre de la operación a efectuar entre ellos: 
-//'suma' ó 'resta' y cargará dinámicamente un módulo para realizar dicho cálculo. 
-
-//operaciones llamará a operacion con los casos de prueba, representando sus salidas.
-
-// operacion deberá devolver el resultado a operaciones a través de una promesa.
-// Los cálculos habilitados estarán definidos en archivos separados empleando clases donde los argumentos de entrada se guardarán en propiedades instancia privadas y tendrán un método 'resultado' que hará efectiva la operación. 
-// Este debe ser un proyecto de typescript que utilice clases, dynamic import, Promises, async await, funciones flecha y tipado en todos sus módulos. 
-// Debe ser compilado para generar un archivo javascript ejecutable por Node.js.
+operaciones();
---
+++ 02-async/resta.js
@@ -0,0 +1,14 @@
+class Resta {
+
+    constructor(numero1, numero2) {
+        this.numero1 = numero1
+        this.numero2 = numero2
+    }
+
+    resultado() {
+        return this.numero1 - this.numero2
+    }
+}
+
+
+module.exports = { Resta }
